87c7634fe60fa1ca02942460f5dedf0a72485b55
Dmitrii @lgt <ushanov.dmitry@gmail.com>
2022-07-01 17:44:06 +0200

    WIP

--- doit/cmd_base.py
+++ doit/cmd_base.py
@@ -136,50 +136,6 @@ class Command(object):
         self.pdb = params.get('pdb', False)
         return self.execute(params, args)
 
-    def help(self):
-        """return help text"""
-        text = []
-        text.append("PURPOSE")
-        text.extend(_wrap(self.doc_purpose, 4))
-
-        text.append("\nUSAGE")
-        usage = "{} {} {}".format(self.bin_name, self.name, self.doc_usage)
-        text.extend(_wrap(usage, 4))
-
-        text.append("\nOPTIONS")
-        options = defaultdict(list)
-        for opt in self.cmdparser.options:
-            options[opt.section].append(opt)
-        for section, opts in sorted(options.items()):
-            section_name = '\n{}'.format(section or self.name)
-            text.extend(_wrap(section_name, 4))
-            for opt in opts:
-                # ignore option that cant be modified on cmd line
-                if not (opt.short or opt.long):
-                    continue
-                text.extend(_wrap(opt.help_param(), 6))
-                # TODO It should always display option's default value
-                opt_help = opt.help % {'default': opt.default}
-                opt_choices = opt.help_choices()
-                opt_config = 'config: {}'.format(opt.name)
-                if opt.env_var:
-                    opt_env = ', environ: {}'.format(opt.env_var)
-                else:
-                    opt_env = ''
-                desc = '{} {} ({}{})'.format(opt_help, opt_choices,
-                                             opt_config, opt_env)
-                text.extend(_wrap(desc, 12))
-
-                # print bool inverse option
-                if opt.inverse:
-                    text.extend(_wrap('--{}'.format(opt.inverse), 6))
-                    text.extend(_wrap('opposite of --{}'.format(opt.long), 12))
-
-        if self.doc_description is not None:
-            text.append("\n\nDESCRIPTION")
-            text.extend(_wrap(self.doc_description, 4))
-        return "\n".join(text)
-
 
 ######################################################################
 
